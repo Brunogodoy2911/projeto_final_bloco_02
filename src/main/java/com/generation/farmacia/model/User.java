f024e857e7ead547c50846c119ed2718d4a48021
Brunogodoy2911 <brunogodoy29@hotmail.com>
2025-05-28 15:11:41 -0300

    :rocket: Deploy do projeto

--- src/main/java/com/generation/farmacia/model/User.java
+++ src/main/java/com/generation/farmacia/model/User.java
@@ -1,18 +1,11 @@
 package com.generation.farmacia.model;
 
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -23,7 +16,7 @@ import lombok.Data;
 @Entity
 @Table(name = "tb_users")
 public class User {
-  
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
--- src/main/java/com/generation/farmacia/model/UserLogin.java
+++ src/main/java/com/generation/farmacia/model/UserLogin.java
@@ -1,5 +1,6 @@
 package com.generation.farmacia.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
@@ -7,8 +8,13 @@ public class UserLogin {
 
   private Long id;
   private String name;
+
+  @Schema(description = "Username para login", example = "brunogodoy123@email.com")
   private String username;
+
+  @Schema(description = "Senha para login", example = "123456")
   private String password;
+
   private String photo;
   private String token;
 }
